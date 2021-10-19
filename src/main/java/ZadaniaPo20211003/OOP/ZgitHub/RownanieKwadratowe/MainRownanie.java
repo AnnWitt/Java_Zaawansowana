e456eb3ea8122a0e466d10e422a54c21a424bdba
Anna Wittmann <anna.wittmann@atos.net>
2021-10-20 01:18:57 +0200

    20211020 01:18

---
+++ src/main/java/ZadaniaPo20211003/OOP/ZgitHub/RownanieKwadratowe/MainRownanie.java
@@ -0,0 +1,14 @@
+package ZadaniaPo20211003.OOP.ZgitHub.RownanieKwadratowe;
+
+public class MainRownanie {
+    public static void main(String[] args) throws Exception {
+
+        QadricEquation.Rownanie(2, 3, 0);
+        QadricEquation.Rownanie(-2, -3, -4);
+        QadricEquation.Rownanie(0, 3, 4);
+        QadricEquation.Rownanie(1, 5, 2);
+        QadricEquation.Rownanie(1, 2, 1);
+
+
+    }
+}
