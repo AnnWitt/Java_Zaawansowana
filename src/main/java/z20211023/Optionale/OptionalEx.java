685472e1b997c1f7a78279965af6b1bb519c25bc
Anna Wittmann <annwitt@tlen.pl>
2021-10-24 11:28:10 +0200

    20211024 11:27

--- src/main/java/z20211023/Optionale/OptionalEx.java
+++ src/main/java/z20211023/Optionale/OptionalEx.java
@@ -12,16 +12,22 @@ public class OptionalEx {
         Optional.of("dd");// przynullu rzuci wyjatkiem
         stringOptional.ifPresent((s -> System.out.println(s))); //consumer
         //ifpresent,isempty itd wywolywane na obiektach
-        stringOptional.orElse("dupa"); //jezeli stringoptional pusty to bedzie dupa
+        stringOptional.orElse("dupa"); //oczekuje stringa
+        //jezeli stringoptional pusty to bedzie dupa
         //najpierw wysietli dupa a potem wartosc stringOptional
+        //jeżeli mamy w nawiasie przerwanie działania to przerwie mimo, że mamy wartosc
         //roznica: w 1 wykona z nawiasu, potem stwierdzi, że jednak wartosc ma i zwroci stringOptional
         //to w nawiasie i tak sie wykona - poprosilam o dane ale w sumie juz mam ;)  -EAGER
         stringOptional.orElseGet(()->"test"); //LAZY
+        //oczekuje suppliera
         //nie wykona sie jezeli nie bedzie potrzeby, najpierw sprawdzi czy jest a potem ew test
         //zwracamy optionle zamiast nulli - dobra praktyka
         //druga dobra praktyka, nie przyjmujemy optionali
         //metody moga zwracac optionale ale nie powinny ich przyjmowac
 
+
+        //jak cos moze byc nullem to w optionala aby uniknac dotarcia do wyjatku
+        //a wyjatek do "zakrycia"
 
     }
 }
